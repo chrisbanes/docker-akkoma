ee000406bce8ed59834176492e7f2c9afa0355b0
Chris Banes <chrisbanes@users.noreply.github.com>
2022-11-22 16:43:05 +0000

    Latest updates

--- build/config.exs
+++ build/config.exs
@@ -2,11 +2,15 @@ import Config
 
 config :pleroma, Pleroma.Web.Endpoint,
   url: [host: System.get_env("DOMAIN", "localhost"), scheme: "https", port: 443],
+  http: [ip: {0, 0, 0, 0}, port: 4000]
+
+config :phoenix, serve_endpoints: true
 
 config :pleroma, :instance,
   name: System.get_env("INSTANCE_NAME", "Akkoma"),
   email: System.get_env("ADMIN_EMAIL"),
-  notify_email: System.get_env("NOTIFY_EMAIL")
+  notify_email: System.get_env("NOTIFY_EMAIL"),
+  static_dir: "/static"
 
 config :pleroma, Pleroma.Repo,
   adapter: Ecto.Adapters.Postgres,
@@ -20,11 +24,11 @@ config :pleroma, Pleroma.Repo,
 config :web_push_encryption, :vapid_details, subject: "mailto:#{System.get_env("NOTIFY_EMAIL")}"
 
 config :pleroma, :database, rum_enabled: false
-config :pleroma, :instance, static_dir: "/var/lib/akkoma/static"
-config :pleroma, Pleroma.Uploaders.Local, uploads: "/var/lib/akkoma/uploads"
+
+config :pleroma, Pleroma.Uploaders.Local, uploads: "/uploads"
 
 # We can't store the secrets in this file, since this is baked into the docker image
-if not File.exists?("/var/lib/akkoma/secret.exs") do
+if not File.exists?("/config/secret.exs") do
   secret = :crypto.strong_rand_bytes(64) |> Base.encode64() |> binary_part(0, 64)
   signing_salt = :crypto.strong_rand_bytes(8) |> Base.encode64() |> binary_part(0, 8)
   {web_push_public_key, web_push_private_key} = :crypto.generate_key(:ecdh, :prime256v1)
@@ -48,16 +52,16 @@ if not File.exists?("/var/lib/akkoma/secret.exs") do
       web_push_private_key: Base.url_encode64(web_push_private_key, padding: false)
     )
 
-  File.write("/var/lib/akkoma/secret.exs", secret_file)
+  File.write("/config/secret.exs", secret_file)
 end
 
-import_config("/var/lib/akkoma/secret.exs")
+import_config("/config/secret.exs")
 
 # For additional user config
-if File.exists?("/var/lib/akkoma/config.exs"),
-  do: import_config("/var/lib/akkoma/config.exs"),
+if File.exists?("/config/config.exs"),
+  do: import_config("/config/config.exs"),
   else:
-    File.write("/var/lib/akkoma/config.exs", """
+    File.write("/config/config.exs", """
     import Config
 
     # For additional configuration outside of environmental variables
